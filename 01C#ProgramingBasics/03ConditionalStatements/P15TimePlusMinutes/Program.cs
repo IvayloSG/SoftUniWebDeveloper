eaf90c0bfff7bb0c3d467084598d137d4e1a7f41
IvayloSG <ivailosg@gmail.com>
2020-08-30 23:54:18 +0300

    Homework

---
+++ 01C#ProgramingBasics/03ConditionalStatements/P15TimePlusMinutes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P15TimePlusMinutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ 01C#ProgramingBasics/03ConditionalStatements/P16ThreeBrothers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P16ThreeBrothers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

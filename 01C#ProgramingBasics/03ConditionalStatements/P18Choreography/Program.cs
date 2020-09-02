8c9716d888dfbff104e632c8a3a37f5d19b44efd
IvayloSG <ivailosg@gmail.com>
2020-09-02 23:44:04 +0300

    Homework

--- 01C#ProgramingBasics/03ConditionalStatements/P18Choreography/Program.cs
+++ 01C#ProgramingBasics/03ConditionalStatements/P18Choreography/Program.cs
@@ -4,9 +4,24 @@ namespace P18Choreography
 {
     class Program
     {
+        private const double maxStepsPercentage = 13;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int steps = int.Parse(Console.ReadLine());
+            int dancers = int.Parse(Console.ReadLine());
+            int days = int.Parse(Console.ReadLine());
+
+            double stepsPerDayPercentage = Math.Ceiling(((double)steps / days) / steps * 100);
+            double stepsPerDancerPercentage = stepsPerDayPercentage / dancers;
+
+            if (stepsPerDancerPercentage <= maxStepsPercentage)
+            {
+                Console.WriteLine($"Yes, they will succeed in that goal! {stepsPerDancerPercentage:f2}%.");
+            }
+            else
+            {
+                Console.WriteLine($"No, they will not succeed in that goal! Required {stepsPerDancerPercentage:f2}% steps to be learned per day.");
+            }
         }
     }
 }
---
+++ 01C#ProgramingBasics/03ConditionalStatements/P19SwimmingRecord/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace P19SwimmingRecord
+{
+    class Program
+    {
+        private const double delayForFefteenMeters = 12.5;
+        static void Main(string[] args)
+        {
+            double worldRecord = double.Parse(Console.ReadLine());
+            double distanceRecord = double.Parse(Console.ReadLine());
+            double secondsPerMeter = double.Parse(Console.ReadLine());
+
+            double toalDelay = Math.Floor((distanceRecord / 15)) * delayForFefteenMeters;
+
+            double time = distanceRecord * secondsPerMeter + toalDelay;
+
+            if (time > worldRecord)
+            {
+                double timeNeeded = time - worldRecord;
+                Console.WriteLine($"No, he failed! He was {timeNeeded:f2} seconds slower.");
+            }
+            else
+            {
+                double timeExcess = worldRecord - time;
+                Console.WriteLine($"Yes, he succeeded! The new world record is {time:f2} seconds.");
+            }
+
+        }
+    }
+}
